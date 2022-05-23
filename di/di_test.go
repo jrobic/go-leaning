02dde3d44d883e09bed09d366496d7847d91bac8
Jonathan Robic <hello@jonathanrobic.fr>
2022-05-23 08:27:30 +0200

    feat: learn dependency injection and mocking

---
+++ di/di_test.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"bytes"
+	"testing"
+)
+
+func TestGreet(t *testing.T) {
+	buffer := bytes.Buffer{}
+	Greet(&buffer, "Jo")
+
+	got := buffer.String()
+	want := "Hello, Jo"
+
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
+}
